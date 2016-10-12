ed1934ceb17f202bd852450ca9768fbf4e39bb71
guptaNswati <sgupta1505@gmail.com>
2016-10-12 22:02:03 +0000

    output to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,13 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+
+/**
+* main - Prints string to standard error
+* Return: 1
+**/
+
+int main(void)
+{
+	perror("and that piece of art is useful - Dora Korpar, 2015-10-19");
+	return (1);
+}
 
