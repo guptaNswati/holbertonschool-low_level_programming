37641a7b4bbf789a57f71acfd58031920c2dfa74
guptaNswati <sgupta1505@gmail.com>
2016-10-12 22:22:40 +0000

    system call to write data out of buffer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
 * main - Prints string to standard error
@@ -7,7 +8,7 @@
 
 int main(void)
 {
-	perror("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-	return (1);
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
 }
 
