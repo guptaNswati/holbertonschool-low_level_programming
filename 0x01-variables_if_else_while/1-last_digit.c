d6b117add11f5f87483a00eff3ce63df11c08935
guptaNswati <sgupta1505@gmail.com>
2016-10-14 01:23:23 +0000

    syntax fixes to 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 
 	if (m > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n , m);
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	}
 	else if (m == 0)
 	{
@@ -25,8 +25,8 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n"
-		       , n, m);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+		       n, m);
 	}
 
 	return (0);
