b40c182ee2d797dd6b2fb9a329ed6abaf9a2701e
guptaNswati <sgupta1505@gmail.com>
2016-10-18 23:31:15 +0000

    fixes

--- 0x02_functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02_functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
-* Prints lowercase alphabets 10 times
+* print_alphabet_x10 - Prints lowercase alphabets 10 times
+* Return: nothing
 **/
 
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	while (count < 10)
+	int outer_loop, inner_loop;
+
+	for (outer_loop = 0; outer_loop < 10; outer_loop++)
 	{
-		print_alphabet();
-		count++;
+		for (inner_loop = 'a'; inner_loop <= 'z'; inner_loop++)
+		{
+			_putchar(inner_loop);
+		}
+		_putchar('\n');
 	}
 }
 
