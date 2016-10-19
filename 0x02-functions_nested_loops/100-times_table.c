cd8807e4c6707cb64c26a07a6b8d45855d46ccbc
guptaNswati <sgupta1505@gmail.com>
2016-10-19 05:25:39 +0000

    fixes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,35 +10,38 @@ void print_times_table(int n)
 {
 	int num, count, product;
 
+	count = 0;
+
 	if (n >= 0 && n <= 15)
 	{
-		count = 0;
 		while (count <= n)
 		{
-			_putchar('0');
 			num = 0;
 			while (num <= n)
 			{
 				product = count * num;
-				_putchar(',');
-				_putchar(' ');
-				if (product  < 10)
+				if (product / 100 == 0 && num != 0)
 				{
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(product + '0');
+					if (product % 100 /10 == 0)
+					{
+						_putchar(' ');
+					}
+					else
+						_putchar(product % 100 / 10 +
+							 '0');
 				}
-				else if (product < 100)
-				{
-					_putchar(' ');
-					_putchar(product / 10 + '0');
-					_putchar(product % 10 + '0');
-				}
-				else
+				else if (product / 100 != 0)
 				{
 					_putchar(product / 100 + '0');
-					_putchar(product % 100 / 10 + '0');
-					_putchar(product % 10 + '0');
+					_putchar(product % 100 / 10 +
+						 '0');
+				}
+				_putchar(product % 10 + '0');
+				if (num != n)
+				{
+					_putchar(',');
+					_putchar(' ');
 				}
 				num++;
 			}
