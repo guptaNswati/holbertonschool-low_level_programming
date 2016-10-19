20f3d655f6559b8d18bd99b636bfdbc335ffcd20
guptaNswati <sgupta1505@gmail.com>
2016-10-19 20:15:35 +0000

    fxing declaration

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,19 +8,20 @@
 
 int main(void)
 {
-int num, limit, result;
-limit = 1024;
-num = 5;
-result = 3;
-while (num < limit)
-{
-if (num % 3 == 0 || num % 5 == 0)
-{
-result += num;
-}
-num++;
-}
+	int num, limit, result;
 
+	limit = 1024;
+	num = 5;
+	result = 3;
+
+	while (num < limit)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			result += num;
+		}
+		num++;
+	}
 
 	printf("%d\n", result);
 	return (0);
