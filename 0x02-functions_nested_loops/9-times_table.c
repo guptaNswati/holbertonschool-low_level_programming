6c34e9d168b84c154e31619bb7ce8f61ae928831
guptaNswati <sgupta1505@gmail.com>
2016-10-19 01:02:05 +0000

    fixes betty

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,23 +16,26 @@ void times_table(void)
 		num = 0;
 		while (num <= 9)
 		{
-			product = count * num
+			product = count * num;
 
-			if (product > 9)
+			if (product < 10)
 			{
+				if (num > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 				digit_2 = product % 10;
 				digit_1 = product / 10;
 				_putchar(digit_1 + '0');
 				_putchar(digit_2 + '0');
-			}
-			else
-			{
-				_putchar(product + '0');
-			}
-			if (num < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
 			}
 			num++;
 		}
