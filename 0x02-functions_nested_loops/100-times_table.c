623ec5972f0847897c05f107056b3400d9edaa4c
guptaNswati <sgupta1505@gmail.com>
2016-10-19 02:27:41 +0000

    fixes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
-* a function that prints the n times table, starting with 0.
+* print_times_table - prints the n times table, starting with 0.
+* @n: the size of table to print to, max 15
 * Return: nothing
 **/
 
@@ -10,60 +10,40 @@ void print_times_table(int n)
 {
 	if (!(n < 0 || n > 15))
 	{
-		int num, count, digit_1, digit_2, digit_3, product;
+		int num, count, product;
 
 		count = 0;
-	
+
 		while (count <= n)
 		{
+			_putchar('0');
 			num = 0;
 			while (num <= n)
 			{
 				product = count * num;
-			
-				if (product > 9 && product < 100)
-				{
-					digit_2 = product % 10;
-					digit_1 = product / 10;
-					_putchar(digit_1 + '0');
-					_putchar(digit_2 + '0');
-				}
-				else if ( product >= 100 && product < 1000)
-				{
-					digit_3 = product % 10;
-					digit_2 = (product % 100) / 10;
-                                        digit_1 = product / 100;
-					_putchar(digit_1 + '0');
-                                        _putchar(digit_2 + '0');
-                                        _putchar(digit_3 + '0');
-				}
+				_putchar(',');
+				_putchar(' ');
 
-				else 
+				if (product  < 10)
 				{
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(product + '0');
-				}	 
-			
-				if (num < n)
-				{
-					_putchar(',');
-					
-					if (product <= 9)
-					{
-						_putchar(' ');
-					        _putchar(' ');
-						_putchar(' ');
-				        }
-					else if (product <= 99)
-					{
-                                                _putchar(' ');
-                                                _putchar(' ');
-					}
-					else
-						_putchar(' ');
 				}
-			
+				else if (product < 100)
+				{
+					_putchar(' ');
+					_putchar(product / 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else
+				{
+					_putchar(product / 100 + '0');
+					_putchar(product % 100 / 10 + '0');
+					_putchar(product % 10 + '0');
+				}
 				num++;
-			} 
+			}
 			_putchar('\n');
 			count++;
 		}
--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,29 +11,29 @@ int main(void)
 	int num_3 = 3;
 	int num_5 = 5;
 	int num_15 = 15;
-	int limit = 1020;
+	int limit = 1024;
 	int sum_3, sum_5, sum_15 = 0;
 	int result;
 
-	while (num_3 <= limit)
+	while (num_3 < limit)
 	{
 		sum_3 += num_3;
 		num_3 += 3;
 	}
 
-	while (num_5 <= limit)
+	while (num_5 < limit)
 	{
 		sum_5 += num_5;
 		num_5 += 5;
 	}
 
-	while (num_15 <= limit)
+	while (num_15 < limit)
 	{
 		sum_15 += num_15;
 		num_15 += 15;
 	}
 
-	result = (sum_3 + sum_5 + 1023) - sum_15;
+	result = (sum_3 + sum_5) - sum_15;
 
 	printf("%d\n", result);
 	return (0);
