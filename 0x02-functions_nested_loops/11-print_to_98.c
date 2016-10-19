e24ba9b656ab92224169acbcdb4860cd23e87c36
guptaNswati <sgupta1505@gmail.com>
2016-10-19 01:55:54 +0000

    fixes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,67 +1,28 @@
+#include <stdio.h>
 #include "holberton.h"
 
 /**
-* a function that prints all natural numbers from n to a given number, 
+* print_to_98 - prints all natural numbers from n to a given number,
 * followed by a new line.
-* Returns: nothing
+* @n: number to start count from
+* Return: nothing
 **/
 
 void print_to_98(int n)
 {
-	
-	int digit_1, digit_2;
-	
-	if (n < 98)
+	if (n <= 98)
 	{
 		while (n < 98)
 		{
-			if (n > 9)
-			{
-			digit_1 = n / 10;
-			digit_2 = n % 10;
-			_putchar(digit_1 + '0');
-			_putchar(digit_2 + '0');
-			}
-		       
-				if (n >= 0 && n <= 9)
-				{
-					_putchar(n + '0');
-				}
-				if (n >= -9 && n <= -1)
-				{
-					_putchar('-');
-					_putchar(n + '0');
-				}
-				if (n < -9 )
-				{
-					_putchar('-');
-					digit_1 = n / 10;
-			        	digit_2 = n % 10;
-					_putchar(digit_1 + '0');
-					_putchar(digit_2 + '0');
-				}
-			
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 			n++;
 		}
 	}
-	else if (n > 98)
-	{
+	else
 		while (n > 98)
 		{
-			digit_1 = n / 10;
-                        digit_2 = n % 10;
-                        _putchar(digit_1 + '0');
-                        _putchar(digit_2 + '0');
-			_putchar(',');
-                        _putchar(' ');
+			printf("%d, ", n);
 			n--;
 		}
-	}
-	digit_1 = n / 10;
-	digit_2 = n % 10;
-	_putchar(digit_1 + '0');
-	_putchar(digit_2 + '0');
-	_putchar('\n');
+	printf("%d\n", n);
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,32 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
-* prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
-* Returns 1439 mins
+* jack_bauer - prints every minute of the day of Jack Bauer,
+* starting from 00:00 to 23:59
+* Return: nothing
 **/
 
 void jack_bauer(void)
 {
-	char start_time;
-	start_time = '0';
+	int start_time, end_time;
 
-	for (
-	while(start_time < 24)
+	start_time = 0;
+
+	while (start_time < 24)
 	{
-		_putchar(start_time);
-		_putchar(
+		end_time = 0;
+
+		while (end_time < 60)
+		{
+			_putchar(start_time / 10 + '0');
+			_putchar(start_time % 10 + '0');
+			_putchar(':');
+			_putchar(end_time / 10 + '0');
+			_putchar(end_time % 10 + '0');
+			_putchar('\n');
+
+			end_time++;
+		}
 		start_time++;
 	}
 }
-
-	
