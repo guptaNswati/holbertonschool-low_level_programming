19489706aef63d4e38c836bac34283316fdc7ee4
guptaNswati <sgupta1505@gmail.com>
2016-10-19 04:21:26 +0000

    for overflow

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
-#include <limits.h>
-
-/* add() declaration */
-unsigned long long int add(unsigned long long int num1, unsigned long long int 
-num2);
 
 /**
 * main - Finds and prints first 98 fibonacci numbers
 * Return: 0
 **/
 
-int main (void)
+int main(void)
 {
-	unsigned long long int num1 = 1;
-	unsigned long long int num2 = 2;
-	int count = 3;
-	unsigned long long int fib_num;
+	int first_count, second_count;
+	long int num1, num2, divide_1, divide_2, fib_num, fib_divide;
 
-	printf("%llu, %llu, ",num1, num2);
-	while (count <= 98)
+	divide_2 = 1;
+	fib_num = 2;
+	num1 = 0;
+	num2 = 0;
+	first_count = 2;
+	second_count = 0;
+
+	printf("%li, %li, ", divide_2, fib_num);
+	while (first_count < 98)
 	{
-		if (fib_num > ULONG_MAX / num2)
+		fib_divide = divide_2 + fib_num;
+		if ((fib_divide / 10000000) > 0)
 		{
-			printf("warning: overflow \n");
+			second_count = fib_divide / 10000000;
+			fib_divide = fib_divide % 10000000;
 		}
+
+		divide_1 = num1 + num2 + second_count;
+
+		if (divide_1 > 0)
+			printf("%li%07li", divide_1, fib_divide);
 		else
-		fib_num = add(num1, num2);
-		printf("fib num %d is %llu, ", count, fib_num);
+			printf("%li", fib_divide);
+
 		num1 = num2;
-		num2 = fib_num;
-	        
-		count++;
+		num2 = divide_1;
+		divide_2 = fib_num;
+		fib_num = fib_divide;
+		second_count = 0;
+
+		if (first_count < 97)
+			printf(", ");
+
+		first_count++;
 	}
-	printf("%lu\n", ULONG_MAX);
 	printf("\n");
 	return (0);
 }
-
-/* function returns the sum of two numbers */
-unsigned long long int add(unsigned long long int num1, unsigned long long int
- num2)
-{
-	unsigned long long int sum;
-	
-	sum = num1 + num2;
-
-	return sum;
-}
- 
