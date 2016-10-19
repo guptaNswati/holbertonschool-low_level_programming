1f5a3f7dd2370b4ec79f7b47d50beb91492a32d7
guptaNswati <sgupta1505@gmail.com>
2016-10-19 05:51:40 +0000

    output fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,23 +11,23 @@ int main(void)
 
 	num1 = 0;
 	num2 = 1;
-	fib_num = 0;
 
 	int count;
 
-	count = 2;
+	count = 0;
 	while (count < 50)
 	{
 		fib_num = num1 + num2;
-		printf("%ld", fib_num);
-		if (count != 49)
-		{
-			printf(", ");
-		}
 		num1 = num2;
 		num2 = fib_num;
+
+		if (count != 49)
+		{
+			printf("%ld, ", fib_num);
+		}
+		else
+			printf("%ld\n", fib_num);
 		count++;
 	}
-	printf("\n");
 	return (0);
 }
