487c5cf7e2034b81ea9172cc461a14f067bbd5a5
guptaNswati <sgupta1505@gmail.com>
2016-10-20 21:28:03 +0000

    minor optimization to prime factor

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -7,23 +7,26 @@
 
 int main(void)
 {
-	long int num1, test_num;
+	long int num1, test_num, least_factor;
 
 	num1 = 2;
+	least_factor = 2;
 	test_num = 612852475143;
 
 	while (test_num > num1)
 	{
 		if (test_num % num1 == 0)
 		{
+			least_factor = 2;
 			test_num = test_num / num1;
-			num1 = 2;
+			num1 = num1 * num1;
 		}
 		else
 		{
 			num1++;
 		}
 	}
-	printf("%ld\n", num1);
+	least_factor = num1;
+	printf("%ld\n", least_factor);
 	return (0);
 }
