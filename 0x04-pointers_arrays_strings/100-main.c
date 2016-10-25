34278b60b5ebe202e796f2a8982bdc88353085b8
guptaNswati <sgupta1505@gmail.com>
2016-10-25 00:33:24 +0000

    parsing string

---
+++ 0x04-pointers_arrays_strings/100-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = _atoi("98");
+	printf("%d\n", n);
+	return (0);
+}
