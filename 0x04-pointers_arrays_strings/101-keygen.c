01c1e2f90cc794e571250e0444e58bfce8e80bd1
guptaNswati <sgupta1505@gmail.com>
2016-10-26 03:53:03 +0000

    fixed string terminating

--- 0x04-pointers_arrays_strings/101-keygen.c
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -1,49 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
 /**
-* main - generates random valid passwords for the program 101-crackme
+* main - generates random valid passwords for the program 101-crackme,
+* sum = 2772
 * Return: 0
 **/
 
 int main(void)
 {
-	int i, l_limit, u_limit, sum, last;
+	int i, l_limit, u_limit, sum;
 	char last_c;
-	time_t t;
+
 	char alphnum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char randStr[20];
-	char str2[21];
-	l_limit = 0;
+	char randStr[40];
+
+	l_limit = 2772 - 122;
 	u_limit = 2772;
 	sum = 0;
 	i = 0;
-	srand((unsigned) time(&t));
+	srand(time(NULL));
 
-	while (sum <= (u_limit - 122))
+	while (sum < l_limit)
 	{
-		randStr[l_limit] = alphnum[rand() % (sizeof(alphnum) -1)];
-		sum += randStr[l_limit];
-		printf("%c\n", randStr[l_limit]);
-		l_limit++;
-	}
-
-	last = u_limit - sum;
-	last_c = (char)last;
-
-	while (i <= 20)
-	{
-		str2[i] = randStr[i];
+		randStr[i] = alphnum[rand() % (sizeof(alphnum) - 1)];
+		sum += randStr[i];
 		i++;
 	}
-	str2[21] = last_c;
 
-	printf("sum is %d\n", sum);
-	printf("rem %d\n", last);
-	printf("char %c\n", last_c);
-	printf("%s \n", str2);
+	last_c = u_limit - sum;
+	randStr[i + 1] = last_c;
+	randStr[i] = 0;
+
+	printf("%s\n", randStr);
 	return (0);
 }
-		
