abdc009b26ff8f8d13bd9335f7023c4df66730b2
guptaNswati <sgupta1505@gmail.com>
2016-10-26 01:13:30 +0000

    fixes

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,7 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < (n - 1); i++)
 	{
 		printf("%d, ", a[i]);
 	}
--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,28 @@
-#include <string.h>
 #include "holberton.h"
+
+/**
+* _strlen - returns the length of a string.
+* @s: a pointer to a char
+* Return: length of a string
+**/
+
+int _strlen(char *s)
+{
+	int length, i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	length = i;
+	return (length);
+}
 
 /**
 * *_strcpy - copies the string pointed to by src, including
 * the terminating null byte (\0), to the buffer pointed to by dest
-* @*dest: a pointer to destination int
-* @*src: a pointer to src int
+* @dest: a pointer to destination int
+* @src: a pointer to src int
 * Return: the pointer to dest
 **/
 
@@ -13,12 +30,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 	char *strP;
-	int length = strlen(src);
+	int length = _strlen(src);
 
 	for (i = 0; i < length; i++)
 	{
 		dest[i] = src[i];
 	}
 	strP = dest;
-	return(strP);
+	return (strP);
 }
