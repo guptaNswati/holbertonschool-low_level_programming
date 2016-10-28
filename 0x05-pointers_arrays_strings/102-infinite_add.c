0cd9751c9a3c6da82b5ba99fc5398c40fbafb69f
guptaNswati <sgupta1505@gmail.com>
2016-10-28 04:56:43 +0000

    addition algo for 102-infinite

---
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* *infinite_add - adds two numbers
+* @n1: pointer to number1
+* @n2: pointer to number2
+* @r: buffer to store result
+* @size_r: buffer size
+* Return: pointer to the result
+**/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, k, j, h, n2_s, n1_s, m;
+
+	i = 0;
+	m = 0;
+	k = 0;
+	j = 0;
+	h = 0;
+	n2_s = 0;
+	n1_s = 0;
+
+	while (n1[j] != '\0')
+	{
+		j++;
+	}
+	n1_s = j - 1;
+	while (n2[h] != '\0')
+	{
+		h++;
+	}
+	n2_s = h - 1;
+	for (; size_r > 0; size_r--)
+	{
+		m = (n1[n1_s] - '0') + (n2[n2_s] - '0') + k;
+		printf("m %d\n", m);
+		if (m > 9)
+		{
+			r[size_r] = (m % 10) + '0';
+			k = m / 10 ;
+			printf("k %d\n", k);
+		}
+		else if (m < 9 && m >= 0)
+		{
+			r[size_r] = m + '0';
+		}
+		n1_s--;
+		n2_s--;
+		/*	i++; */
+	}
+	r[0] = k + '0';
+	/*r[i] = '\0'; */
+
+	printf("r is %s\n", r);
+	return (r);
+}
