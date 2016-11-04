4362790d9afd9ecd45a7b305ac9656ed19335ffd
guptaNswati <sgupta1505@gmail.com>
2016-11-04 07:21:06 +0000

    finally success

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -12,8 +12,8 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '\0')
 		return (0);
-	if (*s1 != *s2 && *s2 == '*')
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	if (*s1 == '\0')
 	{
 		if (*s2 != '*')
