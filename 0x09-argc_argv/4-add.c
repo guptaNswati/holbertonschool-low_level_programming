1d22e2fbaadb0a2d6403f011d4b552a7314bc302
guptaNswati <sgupta1505@gmail.com>
2016-11-06 21:05:53 +0000

    handling negative numbers

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -15,12 +15,15 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		num = atoi(argv[i]);
-		if (num <= 0)
+		if (num == 0)
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += num;
+		if (num > 0)
+		{
+			sum += num;
+		}
 	}
 	printf("%d\n", sum);
 	return (0);
