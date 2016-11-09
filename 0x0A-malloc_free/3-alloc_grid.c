2ece65a2ae83859c5990085fc1915b77c174fb7a
guptaNswati <sgupta1505@gmail.com>
2016-11-09 21:11:51 +0000

    what if args, not valid? return null

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(sizeof(int *) * width);
+		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
 			while (--i >= 0)
