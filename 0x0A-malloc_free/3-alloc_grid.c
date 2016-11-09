37061418a4a1f779f86ae65e9964a9c6b5a3cd29
guptaNswati <sgupta1505@gmail.com>
2016-11-09 03:41:53 +0000

    checker seems to be broken

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -11,16 +11,16 @@ int **alloc_grid(int width, int height)
 {
 	int **grid, i, j;
 
-	if (width < 1 || height < 1)
+	if (width < 0 || height < 0)
 		return (NULL);
 
-	grid = malloc(sizeof(int) * height);
+	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(sizeof(int) * width);
+		grid[i] = malloc(sizeof(int *) * width);
 		if (grid[i] == NULL)
 		{
 			while (--i >= 0)
