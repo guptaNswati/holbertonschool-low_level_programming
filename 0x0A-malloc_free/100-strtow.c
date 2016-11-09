f4220df1cba1a70e209131492d418eafe59a4c9b
guptaNswati <sgupta1505@gmail.com>
2016-11-09 07:52:47 +0000

    word couting recurse

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -7,34 +7,54 @@
 **/
 int word_counter(char *str)
 {
-	int i, words;
-
-	i = words = 0;
-	while (str[i] != '\0')
-        {
-		if (str[i] != ' ' && str[i + 1] != ' ')
-		{
-			return (1);
-		}
-		else if (str[i] == ' ' && str[i + 1] != ' ')
-		{
-			words += 1;
-		}
-		i++;
-	}
-	return (words);
+	if (str[0] != ' ')
+		return (1 + word_counterRec(str, 0));
+	return (word_counterRec(str, 0));
 }
 /**
-* freeRows - free allocated spaces to array rows
-* @strDup: pointer to pointer to char
-* @i: number of rows to be freed
-* Return: nothing
+* word_counterRec - count num of words recursively
+* @str: pointer to char
+* @i: current index
+* Return: number of words
 **/
-void freeRows(char **strDup, int i)
+int word_counterRec(char *str, int i)
 {
-	while (--i >= 0)
-		free(strDup[--i]);
-	free(strDup);
+	if (str[i] == '\0')
+		return (0);
+	if (str[i] == ' ' && str[i + 1] != ' ')
+		return (1 + word_counterRec(str, i + 1));
+	return (word_counterRec(str, i + 1));
+}
+/**
+* createString - creates strings from an array of chars
+* @str: pointer to char
+* @m: cuurent word index in 2d array
+* @twoD: 2d array
+* Return: pointer to char
+**/
+char *createString(char *str, int m, char **twoD)
+{
+	char *res;
+	int j, n, i;
+
+	n = i = 0;
+	while (str[i] != ' ')
+	{
+		i++, n++;
+	}
+	res = malloc(sizeof(char *) * n);
+	if (res == NULL)
+	{
+		while (--m >= 0)
+			free(twoD[--m]);
+		free(twoD);
+		return (NULL);
+	}
+	for (j = 0; j < n; j++, str++)
+		res[j] = *str;
+
+	res[j] = '\0';
+	return (res);
 }
 /**
 * strtow - splits a string into words.
@@ -44,56 +64,42 @@ void freeRows(char **strDup, int i)
 char **strtow(char *str)
 {
 	char **strDup;
-	int i, j, k, n, words;
+	int i, k, n, words;
 
-/*	if (str == '\0')
-	return (NULL); */
 	words = word_counter(str);
-/*	if (words < 1)
-	return (NULL); */
 	strDup = malloc(sizeof(char *) * words);
 	if (strDup == NULL)
 		return (NULL);
-	k = 0;
+
+	if (words == 1)
+	{
+		for (k = 0; str[k] != '\0'; k++)
+			;
+		strDup[0] = malloc(sizeof(char *) * k);
+		if (strDup[0] == NULL)
+		{
+			free(strDup[0]);
+			free(strDup);
+			return (NULL);
+		}
+		n = 0;
+		while (*str != '\0')
+		{
+			strDup[0] = str;
+			n++, str++;
+		}
+		strDup[0][n] = '\0';
+		return (strDup);
+	}
 	for (i = 0; i < words; i++)
 	{
-		while (str[k] != '\0')
+		while (*str != '\0')
 		{
-			n = 0;
-			if (str[k] != ' ' && str[k + 1] != ' ')
-			{
-				strDup[i] = malloc(sizeof(char *) * k);
-				if (strDup[i] == NULL)
-                                {
-                                        freeRows(strDup, i);
-					return (NULL);
-                                }
-				else
-				{
-					strDup[i][n] = str[n];
-                                        n++;
-				}
-
-			}
-			else if (str[k] == ' ' && str[k + 1] != ' ')
-			{
-				strDup[i] = malloc(sizeof(char *) * n);
-                                if (strDup[i] == NULL)
-                                {
-                                        freeRows(strDup, i);
-                                        return (NULL);
-                                }
-                                else
-                                {
-                                        strDup[i][n] = str[n];
-                                        n++;
-                                }
-			}
-			k += n;
-			break;
-			k++;
+			if (*(str) != ' ' && *(str + 1) != ' ')
+				strDup[i] = createString(str, i, strDup);
+			if (strDup[i] == NULL)
+				return (NULL);
 		}
-		strDup[i][n] = '\0';
 	}
 	strDup[i] = '\0';
 	return (strDup);
