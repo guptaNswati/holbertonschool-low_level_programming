8c607ee40144f5e23fdb684d5b7196ebda5eba01
guptaNswati <sgupta1505@gmail.com>
2016-11-10 20:21:34 +0000

    exit for normal terimation

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* malloc_checked - allocates memory using malloc
+* @b: size to be allocated
+* Return: pointer to allocated memory or exit with 98
+**/
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	if (b == 0)
+		exit (98);
+
+	p = malloc(sizeof(void *) * b);
+	if (p == NULL)
+		exit (98);
+	return (p);
+}
