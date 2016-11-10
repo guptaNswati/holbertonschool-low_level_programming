3cfa25b70be6b598e44787e58aea8a5226b87055
guptaNswati <sgupta1505@gmail.com>
2016-11-10 03:23:40 +0000

    something is better than nothing

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -10,7 +10,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *strDup;
-	int i, j, size;
+	int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -20,17 +20,11 @@ char *str_concat(char *s1, char *s2)
 	i = j = 0;
 	while (s1[i] != '\0')
 		i++;
-	i++;
 
 	while (s2[j] != '\0')
 		j++;
-	j++;
 
-	size = i + j;
-	if (size == 0)
-		return (NULL);
-
-	strDup = malloc(sizeof(char) * size);
+	strDup = malloc(sizeof(char) * (i + j + 1));
 	if (strDup == NULL)
 		return (NULL);
 
