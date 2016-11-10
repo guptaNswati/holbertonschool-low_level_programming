b8fc1e0b4a41c7ebb535843a7dfb3021115f4477
guptaNswati <sgupta1505@gmail.com>
2016-11-10 23:30:18 +0000

    fixed the copying of elements

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -9,7 +9,7 @@
 **/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned char *np, *lp;
+	unsigned char *np;
 	unsigned int i;
 
 	if (new_size == old_size)
@@ -19,23 +19,33 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	np = lp = ptr;
-	i = 0;
-	/* previous allocated space */
-	free(ptr);
-	ptr = malloc(new_size * sizeof(void *));
 	if (ptr == NULL)
+	{
+		ptr = malloc(new_size * sizeof(void *));
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	np = malloc(new_size * sizeof(char));
+	if (np == NULL)
 		return (NULL);
+	i = 0;
 	if (new_size > old_size)
 	{
 		while (i < old_size)
-			np++, i++;
-		ptr = lp;
-		return (ptr);
+		{
+			np[i] = ((char *)ptr)[i];
+			i++;
+		}
+		free(ptr);
+		return (np);
 	}
 	/* if new_size < old_size */
 	while (i < new_size)
-		np--, i++;
-	ptr = lp;
-	return (ptr);
+	{
+		np[i] = ((char *)ptr)[i];
+		i++;
+	}
+	free(ptr);
+	return (np);
 }
