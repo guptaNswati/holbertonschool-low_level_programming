b755a778398df741977697674318e480e66d315a
guptaNswati <sgupta1505@gmail.com>
2016-11-14 23:27:49 +0000

    whats in age to float

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -16,10 +16,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		if (d->age > 0)
-			printf("Age: %.1f\n", d->age);
-		else
-			printf("(nil)\n");
+		printf("Age: %.1f\n", d->age);
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
