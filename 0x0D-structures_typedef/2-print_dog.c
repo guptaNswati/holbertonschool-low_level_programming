6a0142ddebde9a55ecc165e48b3113049c608959
guptaNswati <sgupta1505@gmail.com>
2016-11-14 21:03:25 +0000

    missing library can be an error

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -13,15 +13,15 @@ void print_dog(struct dog *d)
 	else
 	{
 		if (d->name != NULL)
-			printf("Name: %s\n", d1->name);
+			printf("Name: %s\n", d->name);
 		else
 			printf("nil\n");
-		if (d->age != 0 || d1->age > 0)
-			printf("Age: %.1f\n", d1->age);
+		if (d->age > 0)
+			printf("Age: %.1f\n", d->age);
 		else
 			printf("nil\n");
 		if (d->owner != NULL)
-			printf("Owner: %s\n", d1->owner);
+			printf("Owner: %s\n", d->owner);
 		else
 			printf("nil\n");
 	}
