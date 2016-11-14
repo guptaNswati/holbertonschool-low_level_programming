c1a718545e9c95e049dd3e576ccf7093ad3fea98
guptaNswati <sgupta1505@gmail.com>
2016-11-14 21:08:17 +0000

    seriously because of brackets

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -15,14 +15,14 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("nil\n");
+			printf("(nil)\n");
 		if (d->age > 0)
 			printf("Age: %.1f\n", d->age);
 		else
-			printf("nil\n");
+			printf("(nil)\n");
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("nil\n");
+			printf("(nil)\n");
 	}
 }
