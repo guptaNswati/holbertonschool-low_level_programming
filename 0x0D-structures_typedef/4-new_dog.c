7f7d73888e75e099c4fddb974713afcd1bf5b2a3
guptaNswati <sgupta1505@gmail.com>
2016-11-15 23:21:05 +0000

    allocating memory for struct elements and copying from variables

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -10,19 +10,35 @@
 **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int nameLen, ownerLen, i;
 	dog_t *d1;
 
 	d1 = (dog_t *)malloc(sizeof(dog_t));
 	if (d1 == NULL)
 		return (NULL);
 
-	d1->name = name;
+	nameLen = ownerLen = 0;
+	while (name[nameLen++])
+		;
+	while (owner[ownerLen++])
+		;
+	d1->name = malloc(nameLen * sizeof(d1->name));
 	if (d1->name == NULL)
+	{
+		free(d1);
 		return (NULL);
+	}
+	for (i = 0; i <= nameLen; i++)
+		d1->name[i] = name[i];
 	d1->age = age;
-	d1->owner = owner;
+	d1->owner = malloc(ownerLen * sizeof(d1->owner));
 	if (d1->owner == NULL)
+	{
+		free(d1->name);
+		free(d1);
 		return (NULL);
-
+	}
+	for (i = 0; i <= ownerLen; i++)
+		d1->owner[i] = owner[i];
 	return (d1);
 }
