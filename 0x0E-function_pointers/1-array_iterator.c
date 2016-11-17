fd5469df0075fdfbb99c5e7f47179d875cd3678c
guptaNswati <sgupta1505@gmail.com>
2016-11-17 19:22:59 +0000

    null checks

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -12,9 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array == NULL || size == 0 || action == NULL)
-		;
-	else
+	if (action != NULL)
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);
