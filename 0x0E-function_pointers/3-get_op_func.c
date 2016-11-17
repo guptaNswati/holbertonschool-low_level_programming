e92dffa6e0752983aa3f623dabe4e0dbc5376f09
guptaNswati <sgupta1505@gmail.com>
2016-11-17 07:39:50 +0000

    first draft of operator function

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,8 @@
+/**
+* get_op_func - selects the correct function to perform requested operator
+* @s: pointer to requested operator
+* Return: pointer to requested operator function
+**/
+int (*get_op_func(char *s))(int, int)
+{
+}
