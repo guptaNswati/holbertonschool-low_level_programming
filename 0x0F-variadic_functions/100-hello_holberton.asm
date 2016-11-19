2bc5f6c1946607d458ce21a6446a7cb89a1d50ce
guptaNswati <sgupta1505@gmail.com>
2016-11-19 08:11:52 +0000

    write with interrup

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,16 @@
-global main	; entry point
+section .text	; code section
+	global main	; entry point
 
-section	.text
 main:
-	mov rax, 1	; write
-	mov rdi, 1	; stdout
-	mov rsi, message	; message address
-	mov rdx, 17	; message length
-	syscall
+	mov	ebx, 1	; write
+	mov 	eax, 4	; stdout
+	mov 	ecx, message	; message address
+	mov 	edx, 17	; message length
+	int 	0x80	; interrupt 80 hex, call kernel
 
-	mov rax, 60	; system exit
-	xor rdi, 0	; return 0
-	syscall
+	mov 	ebx, 0	; exit code
+	xor 	eax, 1	; output to kernel
+	int 	0x80
 
-section .data
-	message: db 'Hello, Holberton', 0x0A 	; message with new line
+section .data 	; data section
+	message:	db 'Hello, Holberton', 0x0A 	; message with new line
