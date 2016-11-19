b89d479932c9ef3a8bc3e11407332c60af6a310e
guptaNswati <sgupta1505@gmail.com>
2016-11-19 08:29:21 +0000

    better format and comments

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,22 @@
+;  assemble:	nasm -f elf64 100-hello_holberton.asm
+;  link: 	gcc 100-hello_holberton.o
+;  run:	        100-hello_holberton.asm
+;  output is:	Hello Holberton, followed by a new line
+
+section .data 	; data section
+	message:	db 'Hello, Holberton', 0x0A 	; message with new line
+
 section .text	; code section
 	global main	; entry point
 
 main:
+
+	mov	edx, 17
+	mov 	ecx, message
 	mov	ebx, 1	; write
 	mov 	eax, 4	; stdout
-	mov 	ecx, message	; message address
-	mov 	edx, 17	; message length
-	int 	0x80	; interrupt 80 hex, call kernel
+	int 	0x80	; interrupt 80 hex, call made to OS directly
 
 	mov 	ebx, 0	; exit code
 	xor 	eax, 1	; output to kernel
 	int 	0x80
-
-section .data 	; data section
-	message:	db 'Hello, Holberton', 0x0A 	; message with new line
