dece96833fb4639c530a80dbbe45d022cdd37d25
guptaNswati <sgupta1505@gmail.com>
2016-11-28 23:14:26 +0000

    using __attribute__((constructor))

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* __attribute__ - this keyword allows to specify special attributes when making
+* a declaration, followed by attribute specification inside double parentheses.
+* refer https://gcc.gnu.org/onlinedocs/gcc-4.3.0/gcc/Function-Attributes.html
+* Here, the constructor attribute causes the function to be called
+* automatically before execution enters main ()
+* be4mian - function that prints a string before main is executed
+**/
+void __attribute__((constructor)) be4main()
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
