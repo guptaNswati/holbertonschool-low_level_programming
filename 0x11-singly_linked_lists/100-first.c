7e25fdf4bfdbf262d0daa142843ca330e5df82f4
guptaNswati <sgupta1505@gmail.com>
2016-11-28 23:44:56 +0000

    rewriting description of 100-first

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-* __attribute__ - this keyword allows to specify special attributes when making
-* a declaration, followed by attribute specification inside double parentheses.
-* refer https://gcc.gnu.org/onlinedocs/gcc-4.3.0/gcc/Function-Attributes.html
-* Here, the constructor attribute causes the function to be called
+* be4main - function that prints a string before main is executed,
+* keyword (__attribute__) allows to specify special attributes when making
+* a declaration, followed by attribute specification inside double parentheses,
+* here, the constructor attribute causes the function to be called
 * automatically before execution enters main ()
-* be4main - function that prints a string before main is executed
+* refer https://gcc.gnu.org/onlinedocs/gcc-4.3.0/gcc/Function-Attributes.html
 **/
 void __attribute__((constructor)) be4main()
 {
