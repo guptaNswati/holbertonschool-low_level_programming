0695d2b8c4d3cf3c1ea02702820a1749c55a9445
guptaNswati <sgupta1505@gmail.com>
2016-11-28 20:32:09 +0000

    free that malloced string

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -11,13 +11,11 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	if (head == NULL)
-		return;
-
 	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
+		free(temp->str);
 		free(temp);
 	}
 	head = NULL;
