cba68142fc03a8801286b4a9c10fa941bae1d031
guptaNswati <sgupta1505@gmail.com>
2016-12-12 00:03:48 +0000

    changed numbers with macros

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 /* create using open in 1 step */
-	crt = open(filename, O_WRONLY | O_CREAT | o_TRUNC, 00600);
+	crt = open(filename, O_WRONLY | O_CREAT | o_TRUNC, S_IRUSR | S_IWUSR);
 	if (crt == -1)
 		return (-1);
 
--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #define BUFRSIZE 1204
+#define ER STDERR_FILENO
 
 /**
 * copy_file - copies the content of a file to another file
@@ -18,38 +19,41 @@ void copy_file(const char *file1, char *file2)
 	ssize_t count, count_out;
 	int f1, f2, fcls1, fcls2;
 	char bufr[BUFRSIZE];
+	mode_t mode;
 
 	f1 = open(file1, O_RDONLY);
 	if (f1 == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", file1);
+		dprintf(ER, "Error: Can't read from file %s\n", file1);
 		exit(98);
 	}
-	f2 = open(file2, O_WRONLY | O_CREAT, 00664);
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	f2 = open(file2, O_WRONLY | O_CREAT, mode);
 	if (f2 == -1)
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", file2), exit(99);
+		dprintf(ER, "Error: Can't write to %s\n", file2), exit(99);
 	count = 1;
 	while (count > 0)
 	{
 		count = read(f1, bufr, BUFRSIZE);
-		if (count == -1);
+		if (count == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", file1);
+			dprintf(ER, "Error: Can't read from file %s\n", file1);
 			exit(98);
 		}
 		count_out = write(f2, bufr, count);
 		if (count_out == -1 || count_out != count)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", file2);
+			dprintf(ER, "Error: Can't write to %s\n", file2);
 			exit(99);
 		}
 	}
 	fcls1 = close(f1);
 	if (fcls1 == -1)
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fcls1), exit(100);
+		dprintf(ER, "Error: Can't close fd %d\n", fcls1), exit(100);
 	fcls2 = close(f2);
 	if (fcls2 == -1)
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fcls2), exit(100);
+		dprintf(ER, "Error: Can't close fd %d\n", fcls2), exit(100);
 }
 
 /**
@@ -62,18 +66,18 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(ER, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if(argv[1] == NULL)
+	if (argv[1] == NULL)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", file1);
-                exit(98);
+		dprintf(ER, "Error: Can't read from file %s\n", file1);
+		exit(98);
 	}
 	if (argv[2] == NULL)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", file2);
-                exit(99);
+		dprintf(ER, "Error: Can't read from file %s\n", file2);
+		exit(99);
 	}
 	copy_file(argv[1], argv[2]);
 	return (0);
