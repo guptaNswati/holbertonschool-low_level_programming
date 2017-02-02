31fb00c5fad36455c85e6ea72e95d72502151330
guptaNswati <sgupta1505@gmail.com>
2017-02-02 06:13:59 +0000

    fix formatting

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -15,20 +15,13 @@ void hash_table_print(const hash_table_t *ht)
 		printf("{");
 		for (i = 0; i < ht->size; i++)
 		{
-			if (ht->array[i])
+			tmp = ht->array[i];
+			while (tmp)
 			{
-				tmp = ht->array[i];
-				while (tmp)
-				{
-					if (i < ht->size - 1)
-						printf("'%s': '%s', ", tmp->key,
-						       tmp->value);
-					/* print format for last node */
-					else if (i == ht->size - 1 && !tmp->next)
-						printf("'%s': '%s'", tmp->key,
-						       tmp->value);
-					tmp = tmp->next;
-				}
+				printf("'%s': '%s'", tmp->key, tmp->value);
+				if (tmp->next)
+					printf(", ");
+				tmp = tmp->next;
 			}
 		}
 		printf("}\n");
