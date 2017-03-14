2333381d4d48b0e4705863cd4235b0279064c0e3
guptaNswati <sgupta1505@gmail.com>
2017-03-14 17:29:08 +0000

    traversals

--- 0x1C-binary_trees/mains/12-main.c
+++ 0x1C-binary_trees/mains/12-main.c
@@ -13,17 +13,17 @@ int main(void)
 	size_t leaves;
 
 	root = binary_tree_node(NULL, 98);
-	root->left = binary_tree_node(root, 12);
+/*	root->left = binary_tree_node(root, 12);
 	root->right = binary_tree_node(root, 402);
 	binary_tree_insert_right(root->left, 54);
-	binary_tree_insert_right(root, 128);
+	binary_tree_insert_right(root, 128); */
 	binary_tree_print(root);
 
 	leaves = binary_tree_leaves(root);
 	printf("Leaves in %d: %lu\n", root->n, leaves);
-	leaves = binary_tree_leaves(root->right);
+/*	leaves = binary_tree_leaves(root->right);
 	printf("Leaves in %d: %lu\n", root->right->n, leaves);
 	leaves = binary_tree_leaves(root->left->right);
-	printf("Leaves in %d: %lu\n", root->left->right->n, leaves);
+	printf("Leaves in %d: %lu\n", root->left->right->n, leaves); */
 	return (0);
 }
