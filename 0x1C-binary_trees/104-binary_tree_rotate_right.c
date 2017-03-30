1d089046f45604cf4d3d7c0b976cef6872ea71bf
guptaNswati <sgupta1505@gmail.com>
2017-03-30 19:31:39 +0000

    rotate left

--- 0x1C-binary_trees/104-binary_tree_rotate_right.c
+++ 0x1C-binary_trees/104-binary_tree_rotate_right.c
@@ -1,41 +1,41 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_rotate_left - performs a left-rotation on a binary tree
+* binary_tree_rotate_right - performs a right-rotation on a binary tree
 * @tree: pointer to the root node of the tree to rotate
 * Return: pointer to the new root node of the tree once rotated
 **/
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *tmp, *tree_p, *left_node;
-	int for_left = 0;
+	binary_tree_t *tmp, *tree_p, *right_node;
+	int for_right = 0;
 
 	if (!tree)
 		return (NULL);
 	if (!tree->left && !tree->right)
 		return (tree);
-	if (!tree->right)
+	if (!tree->left)
 		return (tree);
 	/* save ref to parent */
 	tree_p = tree->parent;
 	/* to check if current node is in left subtree or right tree */
-	if (tree_p && tree_p->left->n == tree->n)
-		for_left = 1;
+	if (tree_p && tree_p->right == tree)
+		for_right = 1;
 	/* save the ref to the node to be rotated */
-	tmp = tree->right;
-	/* save ref to current nodes left child */
-	left_node = tmp->left;
-	tree->right = left_node;
-	if (left_node)
-		left_node->parent = tree;
-	tmp->left = tree;
+	tmp = tree->left;
+	/* save ref to tmp nodes right child */
+	right_node = tmp->right;
+	tree->left = right_node;
+	if (right_node)
+		right_node->parent = tree;
+	tmp->right = tree;
 	tree->parent = tmp;
 	if (tree_p)
 	{
-		if (for_left)
-			tree_p->left = tmp;
-		else
+		if (for_right)
 			tree_p->right = tmp;
+		else
+			tree_p->left = tmp;
 	}
 	tmp->parent = tree_p;
 	return (tmp);
