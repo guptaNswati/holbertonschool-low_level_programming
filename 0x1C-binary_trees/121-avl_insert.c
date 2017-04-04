d975512f1836dfa099999c01d14a5347fcc6b749
guptaNswati <sgupta1505@gmail.com>
2017-04-04 00:17:06 +0000

    avl insert

--- 0x1C-binary_trees/121-avl_insert.c
+++ 0x1C-binary_trees/121-avl_insert.c
@@ -8,6 +8,64 @@
 **/
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	(avl_t *)binary_tree_node(*tree, value);
-	
+	int bal_factor;
+	avl_t *new_node, *parent;
+
+	if (!tree)
+		return (NULL);
+	if (!(*tree))/* insert at root */
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+	if (value < (*tree)->n)/* go left */
+	{
+		if (!(*tree)->left)
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			return ((*tree)->left);
+		}
+		new_node = avl_insert(&(*tree)->left, value);
+	}
+	else if (value > (*tree)->n)/* go right */
+	{
+		if (!(*tree)->right)
+		{
+			(*tree)->right = binary_tree_node(*tree, value);
+			return ((*tree)->right);
+		}
+		new_node = avl_insert(&(*tree)->right, value);
+	}
+	else/* already in tree */
+		return (*tree);
+	if (!new_node)
+		return (NULL);
+	parent = new_node->parent;/* already in tree */
+	while (parent)
+	{
+		bal_factor = binary_tree_balance(parent);
+		/* if greater than 1, tree is left skewed */
+		if (bal_factor > 1)
+		{
+			if (value < parent->left->n)/* left-left case */
+				binary_tree_rotate_right(parent);
+			else if (value > parent->left->n)/* left-right case */
+			{
+				parent->left = binary_tree_rotate_left(parent->left);
+				binary_tree_rotate_right(parent);
+			}
+		}
+		if (bal_factor < -1)
+		{
+			if (value > parent->right->n)/* left-right case */
+				binary_tree_rotate_left(parent);
+			else if (value < parent->right->n)/* left-right case */
+			{
+				parent->right = binary_tree_rotate_right(parent->right);
+				binary_tree_rotate_left(parent);
+			}
+		}
+		parent = parent->parent;
+	}
+	return (new_node);
 }
